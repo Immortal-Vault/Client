934851ac82d8aae8203f7b560f576d2030ef0688
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-06-22 18:34:57 +0300

    feat: server health api check

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+﻿import { useState } from "react";
+
+export default function Main() {
+    const [state, setState] = useState('OFF');
+
+    setInterval(async () => {
+        try {
+            const response = await fetch('http://localhost:3001/ping', {
+                method: 'GET'
+            })
+
+            setState(response.statusText);
+        } catch {
+            setState('OFF')
+        }
+    }, 50);
+
+    return (
+        <>
+            <h2>Server response is: {state}</h2>
+        </>
+    )
+}
